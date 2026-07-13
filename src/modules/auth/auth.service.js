91ce2bedffd40716a249bda0bb38ad414d6725db
Mohammed Hasanfatta <hasanfattamd@gmail.com>
2026-07-13 14:37:06 +0530

    env file update

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,11 +1,24 @@
+import { generateResetToken } from "../../common/utils/jwt.utils.js";
 import User from "./auth.model.js";
 
 const register = async ({ name, email, password, role }) => {
     // do user registration
-    const user = await User.findOne({ email });
-    if (user) throw ApiError.conflict("Email already exists");
+    const userExist = await User.findOne({ email });
+    if (userExist) throw ApiError.conflict("Email already exists");
+    const { rawToken, hashedToken } = generateResetToken();
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role,
+        // verificationToken: hashedToken,
+    });
 
-    let userObj;
+    // TODO: send an email to user with token: rawToken
+    const useObj = user.toObject();
+    delete userObj.password;
+    return userObj.verificationToken;
+
     return userObj;
 };
 
